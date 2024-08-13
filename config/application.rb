999e2013ef981d3d1c20dd2f62b4ed05e6bfaa6c
rshipp <github@rshipp.com>
2024-08-13 16:53:18 -0500

    Add configurable site topic

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,8 @@ module LinkOrganizer
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    # Configurable site topic.
+    config.site_topic = ENV['SITE_TOPIC'] || ''
   end
 end
