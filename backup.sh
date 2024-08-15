8ded0b570a6f3a0ab533420023e9ad51e82bc8b6
rshipp <github@rshipp.com>
2024-08-15 15:38:35 -0500

    make backup initializer

--- backup.sh
+++ backup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+mkdir -p backups/sql
 sqlite3 storage/development.sqlite3 .dump > backups/sql/development.sql
 cd backups/sql
+[ -d .git ] || git init
 git add development.sql
 git commit -m 'backup'
