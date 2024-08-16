982fca13b0240766f476db7280747c24df6a66f7
rshipp <github@rshipp.com>
2024-08-16 14:25:46 -0500

    Add source types to homepage, refactor

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,25 @@ module ApplicationHelper
   def markdown(text)
     raw(@@markdown.render(text))
   end
+
+  def source_type_icon(source_type_name)
+    case source_type_name
+    when "academic_article"
+      '🎓'
+    when "book"
+      '📕'
+    when "news_article"
+      '📰'
+    when "audio"
+      '🎧'
+    when "blog_post"
+      '📝'
+    when "social_post"
+      '🗨'
+    when "video"
+      '🎥'
+    else
+      ''
+    end
+  end
 end
