25a43a7a0cf828860819b2d9fb55fda0a6a09901
rshipp <github@rshipp.com>
2024-11-25 13:55:11 -0600

    Add basic queue functionality

--- app/controllers/queued_links_controller.rb
+++ app/controllers/queued_links_controller.rb
@@ -9,14 +9,32 @@ class QueuedLinksController < ApplicationController
   end
 
   def apply
+    if @queued_link.data.present?
+      Link.create!(
+          url: @queued_link.url,
+          **@queued_link.data
+      )
+    else
+      ImportLinkJob.perform_later(@queued_link.url)
+    end
+
+    respond_to do |format|
+      format.html { redirect_to unprocessed_links_url, notice: "Link import started. It may take a few minutes for new links to appear." }
+      format.json { render :show, status: :created, location: unprocessed_links_url }
+    end
   end
 
   def destroy
+    @queued_link.destroy!
+
+    respond_to do |format|
+      format.html { redirect_to queued_links_url, notice: "Link was successfully removed from queue." }
+      format.json { head :no_content }
+    end
   end
 
   private
     def set_queued_link
       @queued_link = QueuedLink.find(params[:id])
     end
-
 end
