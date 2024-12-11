a603c6a16d284ac69fe70e96a6f8593919c587e4
rshipp <github@rshipp.com>
2024-12-10 18:34:40 -0600

    Optimize homepage queries

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -13,4 +13,26 @@ class Tag < ApplicationRecord
   def to_combobox_display
     name
   end
+
+  # Class methods
+  def self.top_topics
+    Tag.top_tags(:topic, 10)
+  end
+
+  def self.top_source_types
+    Tag.top_tags(:source_type, 7)
+  end
+
+  def self.top_tags(category, count)
+    # Returns [[count, tag_name, tag_id]]
+    Link.joins(:tags)
+      .where(tags: {category: category})
+      .group(:name, 'tags.id')
+      .count.sort_by { |k, v| v }
+      .last(count)
+      .reverse
+      .map do |tag_info, count|
+        [count, tag_info[0], tag_info[1]]
+      end
+  end
 end
